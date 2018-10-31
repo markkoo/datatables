94287167c312f327dd2eae933c107ebcd9eb2b21
Mark Koo <koo_cool88@hotmail.com>
2018-11-01 02:54:47 +0800

    ok

--- module/datatable.ts
+++ module/datatable.ts
@@ -16,17 +16,65 @@ interface TableConfig<T> {
     groupBy?: (a: T, b: T) => boolean
     columnDefs: SColumnDefsSettings<T>[],
     objectForStructure: T,
-    originalConfig : DataTables.Settings
+    originalConfig: DataTables.Settings
 }
 
 export function setupDataTable<T>(config: { elementId: string, config: TableConfig<T> }) {
     const tableConfig = config.config;
-    tableConfig.columnDefs.forEach(c=>{
-        if(c.filter || c.searchNearest){
+    tableConfig.columnDefs.forEach(c => {
+        if (c.filter || c.searchNearest) {
             c.originalColumnDef.orderable = false;
         }
     });
-    const $table = $(document.getElementById(config.elementId)).DataTable({
+
+    const tableElement = document.getElementById(config.elementId);
+    tableElement.classList.add(config.elementId);
+    const $table = $(tableElement).on('init.dt', function () {
+        const searchNearestIndexes = tableConfig.columnDefs.filter(c => c.searchNearest).map(c => (c.originalColumnDef.targets as [number])[0]);
+        const filterIndexes = tableConfig.columnDefs.filter(c => c.filter).map(c => (c.originalColumnDef.targets as [number])[0]);
+        const createInput = (indexes: number[], type: 'searchNearest' | 'filter'): void => {
+            indexes.forEach(columnIndex => {
+                const thisColumnInput = document.createElement('input');
+                thisColumnInput.type = 'text';
+                thisColumnInput.addEventListener('input', e => {
+                    if (type === 'searchNearest') {
+                        console.log(1)
+                        [{ }]
+                        
+                        const columnDatas = $table.column(columnIndex).data();
+                        // const mapedTableDatas = columnDatas.map(function (value) {
+                        //     return Math.abs(value - +(thisColumnInput.value));
+                        // });
+                        // const nearestValue = Math.min.apply(null, mapedTableDatas);
+                        // const rowIndex = mapedTableDatas.indexOf(nearestValue);
+                        
+                        // console.log(rowIndex)
+
+                        // $table.row(':eq(' + ((rowIndex - 3 <= 0) ? 0 : rowIndex - 3) + ')').scrollTo();
+                        // console.log(3)
+                        // $table.rows().deselect();
+                        // console.log(4)
+                        // $table.row(':eq(' + rowIndex + ')').select();
+                        // console.log(5)
+                    }
+                    else {
+                        $table.column(columnIndex).search(thisColumnInput.value).draw();
+                    }
+                });
+                thisColumnInput.addEventListener('focus', e => {
+                    document.querySelectorAll<HTMLInputElement>(`table.${config.elementId} thead input`).forEach(input => {
+                        if (input !== thisColumnInput) input.value = '';
+                    });
+                    $table.column(columnIndex).order('asc').draw();
+                    $table.rows().deselect();
+                });
+                document.querySelector(`table.${config.elementId} thead th:nth-of-type(${columnIndex + 1})`).appendChild(thisColumnInput);
+            })
+        }
+
+        createInput(searchNearestIndexes, 'searchNearest');
+        createInput(filterIndexes, 'filter');
+    }).DataTable({
         ...tableConfig.originalConfig,
         ...{
             ajax: {
@@ -42,51 +90,13 @@ export function setupDataTable<T>(config: { elementId: string, config: TableConf
                         return item;
                     });
                     return (tableConfig.groupBy) ? datasAfterMap.groupBy(tableConfig.groupBy) : datasAfterMap;
-                },
-                success : function(){
-                    const searchNearestIndexes = tableConfig.columnDefs.filter(c => c.searchNearest).map(c => (c.originalColumnDef.targets as [number])[0]);
-                    const filterIndexes = tableConfig.columnDefs.filter(c => c.filter).map(c => (c.originalColumnDef.targets as [number])[0]);
-                    const createInput = (indexes : number[], type : 'searchNearest' | 'filter') : void => {
-                        indexes.forEach(columnIndex => {
-                            const thisColumnInput = document.createElement('input');
-                            thisColumnInput.type = 'text';
-                            thisColumnInput.addEventListener('input', e => {
-                                if(type === 'searchNearest'){
-                                    const columnDatas = $table.column(columnIndex).data();
-                                    const mapedTableDatas = columnDatas.map(function (value) {
-                                        return Math.abs(value - +(thisColumnInput.value));
-                                    });
-                                    const nearestValue = Math.min.apply(null, mapedTableDatas);
-                                    const rowIndex = mapedTableDatas.indexOf(nearestValue);
-                                    
-                                    $table.row(':eq(' + ((rowIndex - 3 <= 0) ? 0 : rowIndex - 3) + ')').scrollTo();
-                                    $table.rows().deselect();
-                                    $table.row(':eq(' + rowIndex + ')').select();
-                                }
-                                else{
-                                    $table.column(columnIndex).search(thisColumnInput.value).draw();
-                                }
-                            });
-                            thisColumnInput.addEventListener('focus', e => {
-                                document.querySelectorAll<HTMLInputElement>('#example th input').forEach(input => {
-                                    if (input !== thisColumnInput) input.value = '';
-                                });
-                                $table.column(columnIndex).order('asc').draw();
-                                $table.rows().deselect();
-                            });
-                            document.querySelector(`#example th:nth-of-type(${columnIndex + 1})`).appendChild(thisColumnInput);
-                        })
-                    }
-        
-                    createInput(searchNearestIndexes,'searchNearest');
-                    createInput(filterIndexes,'filter');
                 }
-                
+
             },
             columnDefs: tableConfig.columnDefs.map(columnDef => columnDef.originalColumnDef)
         }
     });
-   
+
     const clickMethodVsIndex = tableConfig.columnDefs.filter(c => c.click).reduce<{ [name: number]: (row: T) => void }>((result, columnDef) => {
         result[(columnDef.originalColumnDef.targets as [number])[0]] = columnDef.click;
         return result;
@@ -95,7 +105,7 @@ export function setupDataTable<T>(config: { elementId: string, config: TableConf
     document.getElementById(config.elementId).addEventListener('click', e => {
         const targetElement = e.target as HTMLElement;
         let loopElement = targetElement;
-        if(targetElement.dataset['rowData'] === undefined){
+        if (targetElement.dataset['rowData'] === undefined) {
             console.error('Jquery Datatable : click event missing dataset["rowData"]');
         }
         const rowData = JSON.parse(targetElement.dataset['rowData']);
