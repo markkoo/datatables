4ed4890fc5b67b49f9356dc3f9ffb6ea2d0ad993
Derrick Yam <keatkeat87@live.com>
2018-10-31 21:31:30 +0800

    ok

--- Web/home/home.ts
+++ Web/home/home.ts
@@ -8,17 +8,18 @@ import { setupExtension } from '../../module/extensions';
 
 setupExtension();
 
+
+const keys = ['name', 'age', 'like', 'code', 'super', 'lala'];
 let $table = $('#example').DataTable({
     ajax: ({
         url: '/data.json',
         dataSrc: function (data: { data: string[][] }) {
-            const datas = data.data;
-            const keys = ['name', 'age', 'like', 'code', 'super', 'lala'];
-            const result = datas.map((array, index) => {
+            const datas = data.data;            
+            const result = datas.map((array) => {
                 let item: { [name: string ]: any } = {};
-                array.forEach(value => {
+                array.forEach((value, index) => {
                     item[keys[index]] = value;
-                })
+                });
                 return item;
             }).groupBy((a, b) => a['name'] === b['name'] );
             return result;
@@ -44,7 +45,8 @@ let $table = $('#example').DataTable({
             targets: [2],
             render: function (data: any, type: any, row: any, meta: any): any {
                 return row[0]['like']
-            }
+            },
+            orderable : false
         },
         {
             title: 'code',
@@ -58,7 +60,8 @@ let $table = $('#example').DataTable({
             targets: [4],
             render: function (data: any, type: any, row: any, meta: any): any {
                 return row[0]['super']
-            }
+            },
+            orderable : false
         },
         {
             title: 'lala',
@@ -74,10 +77,32 @@ let $table = $('#example').DataTable({
                 return '<button class="enquiryButton">Enquiry</button>'
             }
         }
-
     ],
     initComplete: function (settings, json) {
-        //        
+       
+
+        ['super','like'].forEach(key => {
+
+            const thisColumnInput = document.createElement('input');
+            thisColumnInput.type = 'text';
+            thisColumnInput.addEventListener('input', e => {
+                console.log(thisColumnInput.value); 
+            }); 
+            thisColumnInput.addEventListener('focus', e => {     
+                let ggc = document.querySelectorAll<HTMLInputElement>('#example th input');
+                console.log(ggc);
+                document.querySelectorAll<HTMLInputElement>('#example th input').forEach(input => {
+                    
+                    if(input !== thisColumnInput) input.value == '';
+                });
+                $table.column(keys.indexOf(key)).order('asc').draw();
+            });
+            document.querySelector(`#example th:nth-of-type(${ keys.indexOf(key) + 1 })`).appendChild(thisColumnInput);
+
+        });
+        
+
+
     }
 });
 
